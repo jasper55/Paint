b80e5953c97c14f05a47e96210cf24c23c14905e
Jasper Wagner <jasper_wagner@web.de>
2020-03-09 12:31:16 +0100

    finnish preparing the canvas settings

--- app/src/main/java/wagner/jasper/paint/UI/CustomCanvasView.kt
+++ app/src/main/java/wagner/jasper/paint/UI/CustomCanvasView.kt
@@ -3,6 +3,8 @@ package wagner.jasper.paint.UI
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
 import wagner.jasper.paint.R
@@ -11,7 +13,23 @@ class CustomCanvasView(context: Context): View(context) {
 
     private lateinit var extraCanvas: Canvas
     private lateinit var extraBitmap: Bitmap
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.paintColor,null)
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.background,null)
+
+    private val paint = Paint().apply {
+        color = drawColor
+        // smooth edges of the drawing
+        isAntiAlias = true
+        // samples down color with higher precision
+        isDither = true
+        style = Paint.Style.STROKE //what kind of drawing it is. default: FILL
+        strokeJoin = Paint.Join.ROUND //default MITER
+        strokeCap = Paint.Cap.ROUND  //default: BUTT
+        strokeWidth = STROKE_WIDTH
+    }
+
+    // the pa the user is drawing
+    private var path = Path()
 
     // onSizeChanged is for initilalizing nothing visible happens here
     override fun onSizeChanged(width: Int, height: Int, oldwidth: Int, oldheight: Int) {
@@ -28,4 +46,13 @@ class CustomCanvasView(context: Context): View(context) {
         super.onDraw(canvas)
         canvas.drawBitmap(extraBitmap,0f, 0f,null)
     }
+
+    companion object {
+        const val STROKE_WIDTH = 12f
+
+
+
+    }
+    
+    
 }
