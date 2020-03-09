7c8f7b823b470358a8028b08f81dfcd3c712c0c1
Jasper Wagner <jasper_wagner@web.de>
2020-03-09 12:18:32 +0100

    initiallize custom canvas

--- app/src/main/java/wagner/jasper/paint/MainActivity.kt
+++ app/src/main/java/wagner/jasper/paint/MainActivity.kt
@@ -2,6 +2,7 @@ package wagner.jasper.paint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 import wagner.jasper.paint.UI.CustomCanvasView
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main) will be replaced with custom canvas
         val paintCanvas = CustomCanvasView(this)
+
+        // set canvas to full screen
+        paintCanvas.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        paintCanvas.contentDescription = getString(R.string.paintCanvasDescription)
+        setContentView(paintCanvas)
     }
 }
--- app/src/main/java/wagner/jasper/paint/UI/CustomCanvasView.kt
+++ app/src/main/java/wagner/jasper/paint/UI/CustomCanvasView.kt
@@ -1,11 +1,31 @@
 package wagner.jasper.paint.UI
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
+import wagner.jasper.paint.R
 
 class CustomCanvasView(context: Context): View(context) {
 
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.background,null)
 
+    // onSizeChanged is for initilalizing nothing visible happens here
+    override fun onSizeChanged(width: Int, height: Int, oldwidth: Int, oldheight: Int) {
+        super.onSizeChanged(width, height, oldwidth, oldheight)
+        if(::extraBitmap.isInitialized) extraBitmap.recycle()
+        //onSizeChanged creates always a new bitmap when size is changed, this would cause a memory leak,
+        //  because the old bitmaps would still left around
+        extraBitmap = Bitmap.createBitmap(width,height,Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+    }
 
-
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(extraBitmap,0f, 0f,null)
+    }
 }
