2faa5199ae252aa54dd4a4ace70d2f704e0f5b14
Jasper Wagner <jasper_wagner@web.de>
2020-03-09 11:17:32 +0100

    replace setContentView with custom Canvas

--- app/src/main/java/wagner/jasper/paint/MainActivity.kt
+++ app/src/main/java/wagner/jasper/paint/MainActivity.kt
@@ -2,11 +2,13 @@ package wagner.jasper.paint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import wagner.jasper.paint.UI.CustomCanvasView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main) will be replaced with custom canvas
+        val paintCanvas = CustomCanvasView(this)
     }
 }
