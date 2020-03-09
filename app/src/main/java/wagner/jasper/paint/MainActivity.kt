5295cd4588f01360c270afd340590cb5008dc8d2
Jasper Wagner <jasper_wagner@web.de>
2020-03-09 11:14:00 +0100

    Initial commit

---
+++ app/src/main/java/wagner/jasper/paint/MainActivity.kt
@@ -0,0 +1,12 @@
+package wagner.jasper.paint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main) will be replaced with custom canvas
+    }
+}
---
+++ app/src/main/java/wagner/jasper/paint/UI/CustomCanvasView.kt
@@ -0,0 +1,11 @@
+package wagner.jasper.paint.UI
+
+import android.content.Context
+import android.view.View
+
+class CustomCanvasView(context: Context): View(context) {
+
+
+
+
+}
